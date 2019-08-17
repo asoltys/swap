d5b9ac1356fc41369a9a5c5c6ed3dbd06ffc589a
Adam Soltys <asoltys@gmail.com>
2019-08-17 08:38:57 -0700

    animated prices

--- src/Accept.svelte
+++ src/Accept.svelte
@@ -2,27 +2,37 @@
   let acceptance = "";
   let info = Promise.resolve("");
 
-  const submit = async () => {
-     info = await fetch('/api/acceptance', {
+  async function sendAcceptance() {
+    const res = await fetch('/api/acceptance', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ acceptance })
     })
-    .then(response => response.json());
+		const json = await res.text();
+     
+		if (res.ok) {
+			return json;
+		} else {
+			throw new Error(json);
+		}
+  } 
+
+  const submit = () => {
+    info = sendAcceptance()   
   } 
 </script>
 
-<h1>Accept</h1>
-<textarea bind:value={acceptance}></textarea>
-<button on:click={submit}>Submit</button>
+<textarea class="border p-2 h-64" bind:value={acceptance}></textarea>
+<button class="block bg-blue-600 p-4 text-white mx-auto" on:click={submit}>Submit</button>
 
-<p>err</p>
 {#await info}
-  waiting...
-{:then infotxt}
-  <pre>{JSON.stringify(infotxt)}</pre>
+  Parsing Transaction
+{:then json}
+  {#if json.length > 0}
+    <pre>{JSON.stringify(json)}</pre>
+  {/if}
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
-  import Proposal from "./Proposal.svelte";
+  import { tweened } from 'svelte/motion';
+  import { cubicOut } from 'svelte/easing';
+  import Accept from "./Accept.svelte";
+  import Balance from "./Balance.svelte";
   import assets from "./assets";
 	export let name;
   let showInstructions = false;
   const dismiss = () => showInstructions = false;
-  const submit= () => showInstructions = false;
+  let submitted = false;
 
   let input = assets["tether"];
   let output = assets["bitcoin"];
@@ -14,7 +17,106 @@
     input = output;
     output = temp;
   } 
+
+  let copied = false;
+  const copy = str => {
+    copied = false;
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    const selected =
+      document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    if (selected) {
+      document.getSelection().removeAllRanges();
+      document.getSelection().addRange(selected);
+    }
+    
+    copied = true;
+  };
+
+  let ask = tweened(0, {
+    duration: 400,
+    easing: cubicOut
+  });
+
+  let bid = tweened(0, {
+    duration: 400,
+    easing: cubicOut
+  });
+
+  let last = tweened(0, {
+    duration: 400,
+    easing: cubicOut
+  });
+
+  var ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@ticker');
+  let chanId;
+
+  ws.onmessage = function (event) {
+    let msg = JSON.parse(event.data);
+    ask.set(parseFloat(msg.a));
+    bid.set(parseFloat(msg.b));
+    last.set(parseFloat(msg.l));
+  };
+
+  let proposal = Promise.resolve("");
+  async function getProposal() {
+    const res = await fetch(`/api/proposal?v1=${input.value}&v2=${output.value}&a1=${input.id}&a2=${output.id}`);
+    const json = await res.json();
+
+    if (res.ok) {
+      return json;
+    } else {
+      throw new Error(json);
+    }
+  }
+
+  let state = "home";
+  const submit = async () => {
+    state = "proposing";
+    proposal = getProposal();
+  } 
+
+  let show = false
+  const toggle = () => {
+    copied = false;
+    show = !show;
+  } 
+
+  const download = async () => {
+    copied = false;
+    const p = await proposal;
+    const filename = "proposal.txt";
+    const blob = new Blob([p.proposal], { type: "text/plain;charset=utf-8;" });
+    if (navigator.msSaveBlob) {
+      navigator.msSaveBlob(blob, filename);
+    } else {
+      const link = document.createElement("a");
+      if (link.download !== undefined) {
+        const url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", filename);
+        link.style.visibility = "hidden";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
+  } 
+
+  const accept = () => state = "accepting";
 </script>
+
+<style>
+  p { @apply mb-4; } 
+  p.mb-0 { @apply mb-0; }
+</style>
 
 <div class="flex bg-blue-600 text-white p-5">
   <h1 class="text-3xl"><a href="/">Liquid Swap Server</a></h1>
@@ -23,11 +125,16 @@
 
 {#if showInstructions}
 <div class="p-4 max-w-xl border-b mx-auto">
-  <p class="mb-2">Use this page to request atomic swaps between Bitcoin (BTC) and Tether (USDt) on the Liquid network. Submit the amount you want to swap and the server will generate a proposal transaction. Use the <a class="text-blue-600" href="https://github.com/Blockstream/liquid-swap">liquid-swap tool</a> to accept the proposal, then submit the resulting encoded message with the accepted transaction using the form below.</p>
+  <p>Use this page to request atomic swaps between Bitcoin (L-BTC) and Tether (L-USDt) on the Liquid network. Submit the amount you want to swap and the server will generate a proposal transaction. Use the <a class="text-blue-600" href="https://github.com/Blockstream/liquid-swap">liquid-swap tool</a> to accept the proposal, then submit the encoded acceptance message using the form provided.</p>
 
-  <p class="mb-2">If the exchange rate has not fluctuated out of our favour, we'll immediately finalize and broadcast the transaction. Otherwise, the transaction will be queued until the exchange rate comes back our way.</p>
+  <p>If the exchange rate is in our favour, we'll immediately finalize and broadcast the transaction. Otherwise, the transaction will be queued until the exchange rate comes back our way.</p>
 
-  <p>We use the bid and ask rates from Bitfinex minus a flat fee of <code class="bg-gray-200">0.00002 BTC</code> to cover the transaction fee.</p>
+  <p>We use the bid and ask rates from Binance and don't charge any additional commission.</p>
+
+  <Balance />
+
+	<p class="mb-0">Bid rate: {$bid.toFixed(2)}</p>
+	<p>Ask rate: {$ask.toFixed(2)}</p>
 
   <div class="w-100 text-right">
     <button class="bg-blue-600 p-4 text-white" on:click={dismiss}>Got it</button>
@@ -37,41 +144,65 @@
 
 
 <div class="text-xl max-w-md flex-grow mx-auto p-6 text-center">
-  <h2 class="text-3xl">Swap</h2>
-  <div class="flex">
-    <input type="number" step="any" class="border p-2 flex-grow mr-2 text-right text-2xl" bind:value={input.value} />
-    <img src={`/${input.logo}`} alt={`/${input.alt}`} class="w-12 m-auto cursor-pointer" on:click={swap} />
-  </div>
-  <div class="relative">
-    <img src="/swap.svg" alt="Switch" class="w-12 absolute right-0 cursor-pointer" on:click={swap} />
-    <h2 class="text-3xl">for</h2>
-  </div>
-  <div class="clearfix flex mb-4">
-    <input type="number" step="any" class="border p-2 flex-grow mr-2 text-right text-2xl" bind:value={output.value} />
-    <img  src={`/${output.logo}`} alt={`/${output.alt}`} class="w-12 m-auto cursor-pointer" on:click={swap} />
-  </div>
+  {#if state == "accepting"}
+    <Accept />
+  {/if}
+  {#if state == "proposing"}
+    {#await proposal}
+      Generating Proposal
+    {:then p}
+      {#if p.proposal}
+        <h1 class="text-3xl">The Proposal</h1>
 
-  <button class="bg-blue-400 p-2 text-3xl text-white w-24" on:click={submit}>Go</button>
+        <div class="shadow p-2 rounded text-left my-4">
+          <div>
+            You send: {input.value} {input.name}
+          </div>
+          <div>
+            We send: {output.value} {output.name}
+          </div>
+          <div>
+            We pay this fee: {p.info.legs[0].fee} Bitcoin
+          </div>
+        </div>
+
+        <div>
+          <button class="bg-blue-600 p-4 text-white" on:click={toggle}>{show ? "Hide" : "Show"}</button>
+          <button class="bg-blue-600 p-4 text-white" on:click={() => copy(p.proposal)}>Copy</button>
+          <button class="bg-blue-600 p-4 text-white" on:click={download}>Download</button>
+        </div>
+
+        <button class="bg-blue-600 p-4 text-white my-4" on:click={accept}>Submit Accepted Transaction</button>
+
+        {#if copied}
+          <div>Copied to clipboard!</div>
+        {/if}
+
+        {#if show}
+          <pre class="border bg-gray-100 break-all whitespace-normal text-left mt-4 text-xs p-4">
+            {p.proposal.replace(/\n*|\s*/g, "")}
+          </pre>
+        {/if}
+      {/if}
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+  {/if}
+  {#if state == "home"}
+    <h2 class="text-3xl">Swap</h2>
+    <div class="flex">
+      <input type="number" step="any" class="w-4 border p-2 flex-grow mr-2 text-right text-2xl" bind:value={input.value} />
+      <img src={`/${input.logo}`} alt={`/${input.name}`} class="w-12 m-auto cursor-pointer" on:click={swap} />
+    </div>
+    <div class="relative">
+      <img src="/swap.svg" alt="Switch" class="w-12 absolute right-0 cursor-pointer" on:click={swap} />
+      <h2 class="text-3xl">for</h2>
+    </div>
+    <div class="clearfix flex mb-4">
+      <input type="number" step="any" class="w-4 border p-2 flex-grow mr-2 text-right text-2xl" bind:value={output.value} />
+      <img  src={`/${output.logo}`} alt={`/${output.name}`} class="w-12 m-auto cursor-pointer" on:click={swap} />
+    </div>
+
+    <button class="bg-blue-600 p-2 text-3xl text-white w-24" on:click={submit}>Go</button>
+  {/if}
 </div>
-
-<div class="bg-blue-700 p-4 min-h-screen text-white">
-  <h1 class="text-2xl">Proposals</h1>
-    <table>
-    <thead>
-    <tr>
-      <th>Buy</th>
-      <th>Sell</th>
-      <th>Rate</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>0.1344 BTC</td>
-      <td>0.1344 USDt</td>
-      <td>$10194.14</td>
-    </tr>
-    </tbody>
-    </table>
-</div>
-
-<Proposal />
